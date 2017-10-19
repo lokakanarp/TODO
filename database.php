00b071813efb0d4382f8ebfe348db9de66d056fe
Loka Kanarp <lokakanarp@Lokas-MacBook-Pro.local>
2017-10-19 17:35:55 +0200

    added database.php and style.css

---
+++ database.php
@@ -0,0 +1,10 @@
+ <?php
+       
+//PDO-uppkoppling till databasen
+       $pdo =new PDO(
+        "mysql:host=localhost;dbname=todo_database;charset=utf8", //namet på databasen etc
+    "root",  //user
+    "root"); //password
+    
+
+?>
