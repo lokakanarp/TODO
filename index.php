5e675e40397f13cf92be4182b50e83015ea985fd
Loka Kanarp <lokakanarp@gmail.com>
2017-10-24 12:56:30 +0200

    Fixat layout och bilder samt lagt till lista på gjorda uppgifter. Fixat ORDER by DESC på uppgifterna.

--- index.php
+++ index.php
@@ -5,90 +5,131 @@
     <meta charset="UTF-8">
     <title>TO-DO</title>
 
-    <link rel="stylesheet" href="style.css">
-    <link href="https://fonts.googleapis.com/css?family=Slabo+27px" rel="stylesheet">
+    <link rel="stylesheet" href="style2.css">
+    <link href="https://fonts.googleapis.com/css?family=Roboto+Slab" rel="stylesheet">
 </head>
 
 <body>
-    <main>
-        <form method="POST">
-            <h1>Lokas att göra-lista</h1>
-            <p>Lägg till uppgift:<br>
-                <input type="text" name="title">
-            </p>
-            <p>Skapad av:<br>
-                <input type="text" name="createdBy">
-            </p>
-            <input class="button" type="submit" value="Skicka" />
+    <div class="wrapper">
+        <main>
+            <div class="top_wrap_1">
+                <form method="POST">
+                    <h1>Lokas att göra-lista</h1>
+                    <p>Lägg till uppgift:
+                        <p>
+                            <p><input type="text" name="title"></p>
+                            <p>Skapad av:</p>
+                            <p><input type="text" name="createdBy"></p>
+                            <input class="button" type="submit" value="Skicka" />
 
-        </form>
-        <?php
-        require 'database.php'; 
-        if(isset($_POST["title"])) {
-            $statement = $pdo->prepare(
-                "INSERT INTO todo_list (title, createdBy)
-                VALUES (:title, :createdBy)");
-            $statement->execute(array(
-                ":title" => $_POST["title"],
-                ":createdBy" => $_POST["createdBy"]));
-        }
-        if(isset($_POST["completed"])){
-            $statement = $pdo->prepare(
-                "UPDATE todo_list SET completed = 1 WHERE id = " . $_POST["completed"]
-            );
-            $statement->execute();
-        }
-        if(isset($_POST["delete"])){
-            $statement = $pdo->prepare(
-                "DELETE FROM todo_list WHERE id = " . $_POST["delete"]
-            );
-            $statement->execute();
-        }
-        $todo_list = array();
-        $statement = $pdo->prepare("SELECT * FROM todo_list");
-        $statement->execute();
-        $fetched = $statement->fetchAll(PDO::FETCH_ASSOC);
-        $todo_list = array_reverse($fetched);
-        ?>
+                </form>
+            </div>
+            <div class="top_wrap_2">
+                <img src="cat.jpg">
+            </div>
+            <div class="clear"></div>
 
-        <h2>Att göra:</h2>
-        <table>
-            <thread>
-                <tr>
-                    <th>Nr</th>
-                    <th>Uppgift</th>
-                    <th>Skapad av</th>
-                    <th>Klar</th>
-                    <th>Radera</th>
-                </tr>
-            </thread>
-            <form method="POST">
-                <tbody>
-                    <?php
-            foreach($todo_list as $i){ ?>
+            <?php
+            require 'database.php'; 
+            if(isset($_POST["title"])) {
+                $statement = $pdo->prepare(
+                    "INSERT INTO todo_list (title, createdBy)
+                    VALUES (:title, :createdBy)");
+                $statement->execute(array(
+                    ":title" => $_POST["title"],
+                    ":createdBy" => $_POST["createdBy"]));
+                echo "<p>Din uppgift har framgångsrikt lagts till i listan!</p>";
+            }
+            if(isset($_POST["completed"])){
+                $statement = $pdo->prepare(
+                    "UPDATE todo_list SET completed = 1 WHERE id = " . $_POST["completed"]
+                );
+                $statement->execute();
+
+            }
+            if(isset($_POST["delete"])){
+                $statement = $pdo->prepare(
+                    "DELETE FROM todo_list WHERE id = " . $_POST["delete"]
+                );
+                $statement->execute();
+            }
+            $todo_list = array();
+            $statement = $pdo->prepare("SELECT * FROM todo_list ORDER by id DESC");
+            $statement->execute();
+            $todo_list = $statement->fetchAll(PDO::FETCH_ASSOC);
+            ?>
+
+                <form method="POST">
+                    <h2>Att göra:</h2>
+                    <table>
                         <tr>
-                            <td>
-                                <?php echo $i['id']; ?>
-                            </td>
-                            <td>
-                                <?php echo $i['title']; ?>
-                            </td>
-                            <td>
-                                <?php echo $i['createdBy']; ?>
-                            </td>
-                            <td>
-                                <input type="checkbox" name="completed" value="<?= $i['id']; ?> ">
-                                <input type="checkbox" name="delete" value="<?= $i['id']; ?> "></td>
+                            <th>Nr</th>
+                            <th>Uppgift</th>
+                            <th>Skapad av</th>
+                            <th>Klar</th>
+                            <th>Radera</th>
                         </tr>
                         <?php
-                 }  ?>
-                            <input class="button" type="submit" value="Skicka" />
-                </tbody>
+                foreach($todo_list as $i){ ?>
+                            <tr>
+                                <td>
+                                    <?php echo $i['id']; ?>
+                                </td>
+                                <td>
+                                    <?php if($i['completed'] == 0){ 
+                                    echo $i['title']; 
+                                    } elseif ($i['completed'] == 1) {echo "<s>" . $i['title'] . "</s>" ; }
+                                    ?>
+                                </td>
+                                <td>
+                                    <?php echo $i['createdBy']; ?>
+                                </td>
+                                <td>
+                                    <input type="checkbox" name="completed" value="<?= $i['id']; ?> ">
+                                </td>
+                                <td>
+                                    <input type="checkbox" name="delete" value="<?= $i['id']; ?> "></td>
+                            </tr>
+                            <?php } ?>
+                    </table>
 
-            </form>
-        </table>
-    </main>
-    <footer></footer>
+                    <input class="button" type="submit" value="Skicka">
+                </form>
+                <h2>Utförda uppgifter:</h2>
+                <table>
+                    <tr>
+                        <th>Nr</th>
+                        <th>Uppgift</th>
+                        <th>Skapad av</th>
+                    </tr>
+                    <?php
+                foreach($todo_list as $i){ ?>
+                        <tr>
+                            <td>
+                                <?php if($i['completed'] == 1){
+                                        echo $i['id']; }?>
+                            </td>
+                            <td>
+                                <?php if($i['completed'] == 1){ 
+                                    echo $i['title']; 
+                                    } ?>
+                            </td>
+                            <td>
+                                <?php if($i['completed'] == 1){
+                                          echo $i['createdBy']; }?>
+                            </td>
+                        </tr>
+                        <?php } ?>
+                </table>
+        </main>
+    </div>
+    <div class="footer">
+        <footer>
+            <p class="footer_text">hejhej</p>
+        </footer>
+    </div>
+
+
 </body>
 
 </html>
